f1f44bac7a66717a397429647d682bb1c4e55aa7
Agatha Pires <agathapires11@outlook.com>
2024-03-26 19:08:47 -0300

    Iniciando curso de cypress

---
+++ cypress/e2e/testando.cy.js
@@ -0,0 +1,11 @@
+describe('Página de cadastro', () => {
+  it('passes', () => {
+    cy.visit('https://3076-cypress-alurapic-front.vercel.app/');
+    cy.contains('a', 'Register now').click();
+    cy.get('[data-test="email"]').type('teste@teste.com');
+    cy.get('[data-test="fullName"]').type('Teste');
+    cy.get('[data-test="registerUserName"]').type('teste12');
+    cy.get('[data-test="registerPassword"]').type('senhateste');
+    cy.get('[data-test="btnRegister"]').click();
+  })
+})
