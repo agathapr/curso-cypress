dd52b21f7b2802bc7ad9bb09ce527924d9607bea
Agatha Pires <agathapires11@outlook.com>
2024-03-27 17:33:23 -0300

    Finalizando capitulo 1. Introdução ao Cypress

--- cypress/e2e/testando.cy.js
+++ cypress/e2e/testando.cy.js
@@ -1,5 +1,5 @@
 describe('Página de cadastro', () => {
-  it('passes', () => {
+  it('Preencher os campos do formulário corretamente para cadastrar um novo usuário', () => {
     cy.visit('https://3076-cypress-alurapic-front.vercel.app/');
     cy.contains('a', 'Register now').click();
     cy.get('[data-test="email"]').type('teste@teste.com');
