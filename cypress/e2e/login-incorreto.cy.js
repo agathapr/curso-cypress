67f5c825defc5b8f9653c31737671d1e9682f037
Agatha Pires <agathapires11@outlook.com>
2024-03-28 16:41:07 -0300

    added API tests and methods to protect private info

--- cypress/e2e/login-incorreto.cy.js
+++ cypress/e2e/login-incorreto.cy.js
@@ -2,10 +2,19 @@ describe('Página de login', () => {
 
   beforeEach(() => {
     cy.visit('https://3076-cypress-alurapic-front.vercel.app/')
+
+    cy.intercept('POST', 'https://alurapic-api.onrender.com/user/login', {
+      statusCode: 400
+    }).as('stubPost')
   })
     
   it('Verificar mensagem de campos obrigatórios', () => {
     cy.contains('User name is required!').should('be.visible');
     cy.contains('Password is required!').should('be.visible');
+  })
+
+  it('Deve falhar mesmo que os campos sejam preenchidos corretamente', () => {
+    cy.login('teste12', 'senhateste');
+    cy.wait('@stubPost')
   })
 })
