dd195f5faf962d8657a511d0de2c46278631384e
Agatha Pires <agathapires11@outlook.com>
2024-03-28 15:51:29 -0300

    Finalizando capitulo 3. Mais recursos do Cypress

--- cypress/e2e/login-correto.cy.js
+++ cypress/e2e/login-correto.cy.js
@@ -1,8 +1,10 @@
 describe('Página de cadastro', () => {
-    it('Preencher os campos do formulário corretamente para logar um usuário', () => {
-      cy.visit('https://3076-cypress-alurapic-front.vercel.app/');
-      cy.get('[data-test="loginUserName"]').type('teste123');
-      cy.get('[data-test="loginPassword"]').type('senhateste');
-      cy.get('[data-test="loginBtn"]').click();
-    })
+
+  beforeEach(() => {
+    cy.visit('https://3076-cypress-alurapic-front.vercel.app/')
   })
+
+  it('Preencher os campos do formulário corretamente para logar um usuário', () => {
+      cy.login('teste12', 'senhateste')
+  })
+})
